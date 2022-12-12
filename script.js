7e0c53296f2b07a1085bc7a26e57185659a0c04c
Ashwin  M E <ashwinme.nrp@gmail.com>
2022-12-12 12:15:33 +0530

    First

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function switchon()
+{
+        document.getElementById('bulb').src='./Images/bulb-on.png'  
+}
+
+function switchoff()
+{
+        document.getElementById('bulb').src='./Images/bulb-off.png'  
+}
